8e250b5626c7251ebd4092a355e149e9dfaaf482
Rafael Batista <RafaelB24@yahoo.com>
2019-06-22 17:45:19 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs
+  resources :artists
+  post '/upload', to: 'songs#upload'
+  post '/songs/upload', to: 'songs#upload'
+
+  # resources :songs do
+  #     post '/upload', to: 'songs#upload'
+  # end
+
 end
