820638a2a2db617b1ccd240135056fdec160e97e
github username <github email address>
2019-06-25 07:06:04 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, only: [:index]
+    post 'songs/upload', to: 'songs#upload'
+
+    resources :songs
+    resources :artists
+
 end
