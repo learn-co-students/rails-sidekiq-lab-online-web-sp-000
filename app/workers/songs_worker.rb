140818af201cdf2ac5637bf864514fbc24af0d31
Tom <36858744+decentralvision@users.noreply.github.com>
2019-08-01 14:15:33 -0700

    Done.

---
+++ app/workers/songs_worker.rb
@@ -0,0 +1,9 @@
+class SongsWorker
+    require 'csv'
+    include Sidekiq::Worker
+    def perform(import_file)
+        CSV.foreach(import_file, headers: true) do |song|
+            Song.create(title: song[0], artist_name: song[1])
+        end
+    end
+end
